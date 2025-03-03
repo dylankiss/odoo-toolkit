b67783b9857b93ef8dd0870154bacc659a4240dd
Dylan Kiss (dyki) <dyki@odoo.com>
2025-03-03 09:06:25 +0100

    [IMP] mv: add `switch` command to multiverse

--- odoo_toolkit/mv/__init__.py
+++ odoo_toolkit/mv/__init__.py
@@ -2,10 +2,12 @@ from typer import Typer
 
 from .reset import app as reset_app
 from .setup import app as setup_app
+from .switch import app as switch_app
 
 app = Typer(no_args_is_help=True)
 app.add_typer(setup_app)
 app.add_typer(reset_app)
+app.add_typer(switch_app)
 
 
 @app.callback()
